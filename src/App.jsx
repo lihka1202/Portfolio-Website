2e3534f49058c0bf4d8f2ca1c85864cc9785f352
Akhil Narayanachary <akhil.narayanachary@transcelestial.com>
2024-02-07 11:00:27 +0800

    chore(setup): clean up linting issues
    - Cleaned up the issues related to linting
    - Made sure that prettier and eslint are universally functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import {
-  About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
 } from './components';
 
 function App() {
@@ -24,7 +32,6 @@ function App() {
           <StarsCanvas />
         </div>
       </div>
-
     </BrowserRouter>
   );
 }
