850853ce45f70cca38258510b6b534bf2b99fdcd
lihka1202 <mnakhilwork@gmail.com>
2024-02-11 14:45:03 +0800

    feat(experience): add basic experience section
    - need to add the logos and the images.
    - Finish the verticalTreeLineComponent

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,6 +26,7 @@ import meta from './company/meta.png';
 import shopify from './company/shopify.png';
 import starbucks from './company/starbucks.png';
 import tesla from './company/tesla.png';
+import nus from './company/NUS.png';
 
 import carrent from './carrent.png';
 import jobit from './jobit.png';
@@ -61,4 +62,5 @@ export {
   jobit,
   tripguide,
   akhil,
+  nus,
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-useless-fragment */
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+
+import { motion } from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+import { experiences } from '../constants';
+
+import { SectionWrappingMechanism } from '../hoc';
+
+import { textVariant } from '../utils/motion';
+
+//! Exp Card
+
+const IndivExperienceCard = ({ experience }) => (
+  <VerticalTimeline>
+    contentStyle={{ background: '#1d1836', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid #232631' }}
+    date= {experience.date}
+  </VerticalTimeline>
+);
 
 function Experience() {
   return (
-    <div>Experience</div>
+    //! Fragments are more efficient ways of adding elements to the DOM
+    <>
+      <motion.div>
+        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+          Some companies I have worked for..
+        </p>
+        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+          Experience
+        </h2>
+      </motion.div>
+
+      {/* Vertical Timeline */}
+      <div className=" mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <IndivExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>
   );
 }
 
-export default Experience;
+//! Follow the Navbar for the IdName to make sure routerdom can pick up the right place to point to
+export default SectionWrappingMechanism(Experience, 'work');
