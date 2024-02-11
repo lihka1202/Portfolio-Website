5f8ab127e5ce627878064d0d11853e165c6fc564
lihka1202 <mnakhilwork@gmail.com>
2024-02-11 13:10:23 +0800

    feat(about): add raw about section
    - Need to add a higher order component which ensures even padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,74 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/jsx-no-useless-fragment */
 import React from 'react';
+
+import { Tilt } from 'react-tilt';
+
+import { motion } from 'framer-motion';
+
+import { services } from '../constants';
+
+import { fadeIn, textVariant } from '../utils/motion';
+
+const IndivServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className=" bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          {/* Add the text */}
+          <h3 className=" text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+          Introduction
+        </p>
+        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+          Overview.
+        </h2>
+      </motion.div>
+
+      {/* Use the tbe predefined animation to add some animation to the text */}
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className=" mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Welcome to my corner of the internet! I'm a driven computing student at
+        the National University of Singapore, deeply immersed in the world of
+        technology and innovation. Currently interning at an emerging startup,
+        Transcelestial, I'm honing my skills in software engineering, testing
+        infrastructure, and full-stack app development. Join me on this journey
+        as I navigate through the dynamic realm of tech and embrace new
+        challenges and opportunities along the way.
+      </motion.p>
+
+      <div className=" mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <IndivServiceCard
+            key={service.title}
+            index={index}
+            title={service.title}
+            icon={service.icon}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,11 +46,11 @@ export const navLinks = [
 
 const services = [
   {
-    title: 'Web Developer',
+    title: 'Frontend Developer',
     icon: web,
   },
   {
-    title: 'React Native Developer',
+    title: 'DevOps Engineer',
     icon: mobile,
   },
   {
@@ -58,7 +58,7 @@ const services = [
     icon: backend,
   },
   {
-    title: 'Content Creator',
+    title: 'Quality Assurance',
     icon: creator,
   },
 ];
@@ -176,7 +176,7 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-        'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
     name: 'Sara Lee',
     designation: 'CFO',
     company: 'Acme Co',
@@ -184,7 +184,7 @@ const testimonials = [
   },
   {
     testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
     name: 'Chris Brown',
     designation: 'COO',
     company: 'DEF Corp',
@@ -192,7 +192,7 @@ const testimonials = [
   },
   {
     testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
     name: 'Lisa Wang',
     designation: 'CTO',
     company: '456 Enterprises',
@@ -204,7 +204,7 @@ const projects = [
   {
     name: 'Car Rent',
     description:
-        'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
+      'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
     tags: [
       {
         name: 'react',
@@ -225,7 +225,7 @@ const projects = [
   {
     name: 'Job IT',
     description:
-        'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
+      'Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.',
     tags: [
       {
         name: 'react',
@@ -246,7 +246,7 @@ const projects = [
   {
     name: 'Trip Guide',
     description:
-        'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: [
       {
         name: 'nextjs',
@@ -266,6 +266,4 @@ const projects = [
   },
 ];
 
-export {
-  services, technologies, experiences, testimonials, projects,
-};
+export { services, technologies, experiences, testimonials, projects };
