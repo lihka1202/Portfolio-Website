9db0d2dac453c7681269f016b10570b50be7902a
lihka1202 <mnakhilwork@gmail.com>
2024-02-12 19:54:53 +0800

    feat(works): add works basic code
    - Added the basic code for works
    - Fixed lighting intensity issues for the canvas

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+/* eslint-disable react/jsx-no-useless-fragment */
+import { Tilt } from 'react-tilt';
 
-const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+import { motion } from 'framer-motion';
 
-export default Works
+import { github } from '../assets';
+
+import { SectionWrappingMechanism } from '../hoc';
+
+import { projects } from '../constants';
+
+import { fadeIn, textVariant } from '../utils/motion';
+
+const Works = () => (
+  <>
+    <motion.div variants={textVariant}>
+      <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">
+        Some of my finest works..
+      </p>
+      <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">
+        Projects.
+      </h2>
+    </motion.div>
+  </>
+);
+
+export default SectionWrappingMechanism(Works, '');
