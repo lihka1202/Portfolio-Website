4b228334dfd540304a2ec05868d4598558ed08be
lihka1202 <mnakhilwork@gmail.com>
2024-02-14 16:14:40 +0800

    feat(xwing): added xwing
    - Feel like a little cooler to add this, rather than the other computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,36 +11,36 @@ import { OrbitControls, Preload, meshBounds, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('./xwing/scene.gltf');
 
-  return (
-    <mesh>
-      <hemisphereLight intensity={4} groundColor="black" />
-      {/* Add the pintlight to help */}
-      <pointLight intensity={10} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-    // But this is not enough, add this to the canvas to see it
-  );
   // return (
   //   <mesh>
-  //     <hemisphereLight intensity={1} groundColor="black" />
+  //     <hemisphereLight intensity={4} groundColor="black" />
   //     {/* Add the pintlight to help */}
-  //     <pointLight intensity={1} />
+  //     <pointLight intensity={10} />
   //     <primitive
   //       object={computer.scene}
-  //       scale={0.16}
-  //       position={[-1, -3.3, 0]}
-  //       rotation={[0, 0, 0]}
+  //       scale={isMobile ? 0.7 : 0.75}
+  //       position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+  //       rotation={[-0.01, -0.2, -0.1]}
   //     />
   //   </mesh>
   //   // But this is not enough, add this to the canvas to see it
   // );
+  return (
+    <mesh>
+      <hemisphereLight intensity={3} groundColor="purple" />
+      {/* Add the pintlight to help */}
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.9 : 1.8}
+        position={isMobile ? [0, 0, 0] : [0, -1, 0]}
+        rotation={[0, 1.3, 0]}
+      />
+    </mesh>
+    // But this is not enough, add this to the canvas to see it
+  );
 };
 
 const ComputersCanvas = () => {
@@ -81,6 +81,7 @@ const ComputersCanvas = () => {
         {/* Enabled orbit controlls around the model */}
         <OrbitControls
           enableZoom={false}
+          autoRotate
           // Disable y axis motion
           maxPolarAngle={Math.PI}
           minPolarAngle={Math.PI / 4}
