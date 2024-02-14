ca6f2c4f2cff81f3245592446ab57e4a10ab6cc8
lihka1202 <mnakhilwork@gmail.com>
2024-02-14 12:54:36 +0800

    feat(contact): add earth canvas
    - Add earth canvas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -82,6 +82,14 @@ function Contact() {
           </button>
         </form>
       </motion.div>
+
+      {/* floating earth */}
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
+      </motion.div>
     </div>
   );
 }
